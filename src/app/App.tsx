c0652dece3bdfd0864519f287aa88873f844bd1b
handy <handy.kang.dev@gmail.com>
2025-05-08 21:33:19 +0900

    feat: Tailwind CSS 및 Radix UI 통합
    - Tailwind CSS 및 관련 패키지 추가
    - Radix UI의 Slot 컴포넌트 추가
    - tsconfig.json에서 모듈 해상도 방식을 'bundler'로 변경
    - Vite 설정에서 Tailwind CSS 플러그인 통합
    - App.tsx에서 기존 CSS 파일 제거 및 Tailwind CSS 클래스 사용
    - App.css 파일 삭제
    - index.css 파일에 Tailwind CSS 및 사용자 정의 변수를 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,9 @@
-import { useEffect, useMemo, useState } from "react";
-import "./App.css";
+import { Button } from "@/shared/ui/button";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { Chart } from "./Chart";
-import { useStatistics } from "./useStatistics";
 
 function App() {
-  const staticData = useStaticData();
-  const statistics = useStatistics(10);
   const [networkInfo, setNetworkInfo] = useState<NetworkInfo | null>(null);
-  const [activeView, setActiveView] = useState<View>("CPU");
-  const cpuUsages = useMemo(
-    () => statistics.map((stat) => stat.cpuUsage),
-    [statistics]
-  );
-  const ramUsages = useMemo(
-    () => statistics.map((stat) => stat.ramUsage),
-    [statistics]
-  );
-  const storageUsages = useMemo(
-    () => statistics.map((stat) => stat.storageUsage),
-    [statistics]
-  );
-  const activeUsages = useMemo(() => {
-    switch (activeView) {
-      case "CPU":
-        return cpuUsages;
-      case "RAM":
-        return ramUsages;
-      case "STORAGE":
-        return storageUsages;
-    }
-  }, [activeView, cpuUsages, ramUsages, storageUsages]);
-
-  useEffect(() => {
-    return window.electron.subscribeChangeView((view) => setActiveView(view));
-  }, []);
 
   useEffect(() => {
     window.electron.network.startNetworkMonitoring();
@@ -47,65 +16,25 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="p-20">
+      <div className="bg-blue-200">
+        <div className="bg-primary text-primary-foreground shadow-xs hover:bg-primary/90">
+          asdf
+        </div>
+        <div className="bg-destructive  text-4xl">asdfas</div>
+        <Button>AAAAA</Button>
+        <Button variant={"destructive"} className="bg-red-500 text-4xl">
+          AAAAA
+        </Button>
+      </div>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
       <div className="networkInfo">
         {networkInfo?.isCompanyNetwork ? "Company Network" : "Public Network"}
         {networkInfo?.ssid}
       </div>
       <Header />
-      <div className="main">
-        <div>
-          <SelectOption
-            onClick={() => setActiveView("CPU")}
-            title="CPU"
-            view="CPU"
-            subTitle={staticData?.cpuModel ?? ""}
-            data={cpuUsages}
-          />
-          <SelectOption
-            onClick={() => setActiveView("RAM")}
-            title="RAM"
-            view="RAM"
-            subTitle={(staticData?.totalMemoryGB.toString() ?? "") + " GB"}
-            data={ramUsages}
-          />
-          <SelectOption
-            onClick={() => setActiveView("STORAGE")}
-            title="STORAGE"
-            view="STORAGE"
-            subTitle={(staticData?.totalStorage.toString() ?? "") + " GB"}
-            data={storageUsages}
-          />
-        </div>
-        <div className="mainGrid">
-          <Chart
-            selectedView={activeView}
-            data={activeUsages}
-            maxDataPoints={10}
-          />
-        </div>
-      </div>
     </div>
-  );
-}
-
-function SelectOption(props: {
-  title: string;
-  view: View;
-  subTitle: string;
-  data: number[];
-  onClick: () => void;
-}) {
-  return (
-    <button className="selectOption" onClick={props.onClick}>
-      <div className="selectOptionTitle">
-        <div>{props.title}</div>
-        <div>{props.subTitle}</div>
-      </div>
-      <div className="selectOptionChart">
-        <Chart selectedView={props.view} data={props.data} maxDataPoints={10} />
-      </div>
-    </button>
   );
 }
 
@@ -127,18 +56,6 @@ function Header() {
       />
     </header>
   );
-}
-
-function useStaticData() {
-  const [staticData, setStaticData] = useState<StaticData | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      setStaticData(await window.electron.getStaticData());
-    })();
-  }, []);
-
-  return staticData;
 }
 
 export default App;
